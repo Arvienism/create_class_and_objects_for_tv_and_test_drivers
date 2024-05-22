0774ac8e7284089b1b2b40e0ad29bdda0133fac3
Arvie Lastra <lastraarvie@gmail.com>
2024-05-23 05:06:15 +0800

    Added Pseudocode

---
+++ test_tv.py
@@ -0,0 +1,11 @@
+# import tv.py to this Python file
+#   create the method for turning "on" the tv
+#   create the method for turning "off" the tv
+#   create the method for returning the channel for this tv
+#   create a method for set a channel for this tv
+#   create a method for getting the volume for this tv
+#   create a method for set a volume for this tv 
+#   create a method for increases the number by 1
+#   create a method for decreases the number by 1
+#   increases the volumeup level by 1
+#   decreases the volumedown level by 1
---
+++ tv.py
@@ -0,0 +1,12 @@
+# create a class name tv
+#   create constructor and the parameters
+#   create the method for turning "on" the tv
+#   create the method for turning "off" the tv
+#   create the method for returning the channel for this tv
+#   create a method for set a channel for this tv
+#   create a method for getting the volume for this tv
+#   create a method for set a volume for this tv 
+#   create a method for increases the number by 1
+#   create a method for decreases the number by 1
+#   increases the volumeup level by 1
+#   decreases the volumedown level by 1

3c52ad50e183dc3761ef93fac174a25945251572
Arvie Lastra <lastraarvie@gmail.com>
2024-05-23 05:44:54 +0800

    Added pseudocode

--- test_tv.py
+++ test_tv.py
@@ -1,11 +1,43 @@
 # import tv.py to this Python file
-#   create the method for turning "on" the tv
-#   create the method for turning "off" the tv
-#   create the method for returning the channel for this tv
-#   create a method for set a channel for this tv
-#   create a method for getting the volume for this tv
-#   create a method for set a volume for this tv 
-#   create a method for increases the number by 1
-#   create a method for decreases the number by 1
-#   increases the volumeup level by 1
-#   decreases the volumedown level by 1
+# create an object name tv_1
+# Asks if the user wants to turn on the tv
+#    call the method to turn "on" the tv
+# Asks if the user wants to turn off the tv
+#    call the method to turn "off" the tv
+# Asks if the user wants to return the channel
+#    call the method to return the channel to this tv
+# Asks if the user wants to set a channel
+#    call the method to set a channel to this tv
+# Asks if the user wants to get the volume
+#    call the method to get the volume to this tv
+# Asks if the user wants to set a volume
+#    call the method to set a volume to this tv
+# Asks if the user wants to increase the channel number by 1
+#    call the method to increases the channel by 1
+# Asks if the user wants to decrease the channel number by 1
+#    call the method to decrease the channel by 1
+# Asks if the user wants to increase the volume level by 1
+#    call the method to increases the volume level by 1
+# Asks if the user wants to decrease the volume level by 1 
+#    call the method to decrease the volume level by 1 
+# create an object name tv_2
+# Asks if the user wants to turn on the tv
+#    call the method to turn "on" the tv
+# Asks if the user wants to turn off the tv
+#    call the method to turn "off" the tv
+# Asks if the user wants to return the channel
+#    call the method to return the channel to this tv
+# Asks if the user wants to set a channel
+#    call the method to set a channel to this tv
+# Asks if the user wants to get the volume
+#    call the method to get the volume to this tv
+# Asks if the user wants to set a volume
+#    call the method to set a volume to this tv
+# Asks if the user wants to increase the channel number by 1
+#    call the method to increases the channel by 1
+# Asks if the user wants to decrease the channel number by 1
+#    call the method to decrease the channel by 1
+# Asks if the user wants to increase the volume level by 1
+#    call the method to increases the volume level by 1
+# Asks if the user wants to decrease the volume level by 1 
+#    call the method to decrease the volume level by 1 
--- tv.py
+++ tv.py
@@ -1,12 +1,12 @@
 # create a class name tv
-#   create constructor and the parameters
-#   create the method for turning "on" the tv
-#   create the method for turning "off" the tv
-#   create the method for returning the channel for this tv
-#   create a method for set a channel for this tv
-#   create a method for getting the volume for this tv
-#   create a method for set a volume for this tv 
-#   create a method for increases the number by 1
-#   create a method for decreases the number by 1
-#   increases the volumeup level by 1
-#   decreases the volumedown level by 1
+#   create a constructor and the parameters
+#   create a method to turn "on" the tv
+#   create a method to turn "off" the tv
+#   create a method to return the channel to this tv
+#   create a method to set a channel to this tv
+#   create a method to getting the volume to this tv
+#   create a method to set a volume to this tv 
+#   create a method to increases the channel number by 1
+#   create a method to decreases the channnel number by 1
+#   increases the volume level by 1
+#   decreases the volume level by 1

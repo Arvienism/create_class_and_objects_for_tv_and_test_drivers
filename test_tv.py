809f5bc53378db329b480f3f8e40652ec4dd8868
Arvie Lastra <lastraarvie@gmail.com>
2024-05-23 13:03:21 +0800

    Added space

--- test_tv.py
+++ test_tv.py
@@ -1,7 +1,9 @@
 # import tv.py to this Python file
 from tv import TV
+
 # create an object name tv_1
 tv_1 = TV()
+
 # Asks if the user wants to turn on the tv
 tv_1.turn_on()
 tv_status = input("Do you want to turn on the TV? (y/n): ").lower()
@@ -11,22 +13,30 @@ if tv_status != "y":
 else:
     tv_status = True
     tv_1.turn_on(tv_status)
+    
 # Asks if the user wants to set a channel
 channel = int(input("Enter the channel (1-120): "))
 #    call the method to set a channel to this tv
 tv_1.set_channel(channel)
+
 # Asks if the user wants to set a volume
 #    call the method to set a volume to this tv
+
 # Asks if the user wants to increase the channel number by 1
 #    call the method to increases the channel by 1
+
 # Asks if the user wants to decrease the channel number by 1
 #    call the method to decrease the channel by 1
+
 # Asks if the user wants to increase the volume level by 1
 #    call the method to increases the volume level by 1
+
 # Asks if the user wants to decrease the volume level by 1 
 #    call the method to decrease the volume level by 1 
+
 # create an object name tv_2
 tv_2 = TV()
+
 # Asks if the user wants to turn on the tv
 tv_2.turn_on()
 tv_status = input("Do you want to turn on the TV? (y/n): ").lower()
@@ -36,15 +46,21 @@ if tv_status != "y":
 else:
     tv_status = True
     tv_2.turn_on(tv_status)
+    
 # Asks if the user wants to set a channel
 #    call the method to set a channel to this tv
+
 # Asks if the user wants to set a volume
 #    call the method to set a volume to this tv
+
 # Asks if the user wants to increase the channel number by 1
 #    call the method to increases the channel by 1
+
 # Asks if the user wants to decrease the channel number by 1
 #    call the method to decrease the channel by 1
+
 # Asks if the user wants to increase the volume level by 1
 #    call the method to increases the volume level by 1
+
 # Asks if the user wants to decrease the volume level by 1 
 #    call the method to decrease the volume level by 1 

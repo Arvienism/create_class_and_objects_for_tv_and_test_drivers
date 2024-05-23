cd17fb090198e342273f7dac64d26914e1ab8229
Arvie Lastra <lastraarvie@gmail.com>
2024-05-23 11:39:29 +0800

    Added methods for Channels

--- tv.py
+++ tv.py
@@ -14,10 +14,21 @@ class TV:
         self.status = False
         print(self.status + "The TV is off")
 #   create a method to return the channel to this tv
+    def get_channel(self):
+        return self.channel
 #   create a method to set a channel to this tv
+    def set_channel(self):
+        if 1 <= self.channel <= 120 and self.status:
+            self.channel
 #   create a method to getting the volume to this tv
 #   create a method to set a volume to this tv 
 #   create a method to increase the channel number by 1
+    def channel_up(self):
+        if self.channel < 120 and self.status:
+            self.channel += 1
 #   create a method to decrease the channnel number by 1
+    def channel_down(self):
+        if self.channel > 1 and self.status:
+            self.channel -= 1
 #   create a method to increase the volume level by 1
 #   create a method to decrease the volume level by 1

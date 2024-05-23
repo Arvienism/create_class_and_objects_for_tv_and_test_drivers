8708d70ca803052d0e27d8b8915506e44411eee0
Arvie Lastra <lastraarvie@gmail.com>
2024-05-23 14:21:26 +0800

    Added instance variable

--- tv.py
+++ tv.py
@@ -17,16 +17,16 @@ class TV:
     def get_channel(self):
         return self.channel
 #   create a method to set a channel to this tv
-    def set_channel(self):
+    def set_channel(self, channel):
         if 1 <= self.channel <= 120 and self.status:
-            self.channel
+            self.channel = channel
 #   create a method to getting the volume to this tv
     def get_volume(self):
         return self.volume
 #   create a method to set a volume to this tv 
-    def set_volume(self):
+    def set_volume(self, volume):
         if 1 <= self.volume <= 7 and self.status:
-            self.volume
+            self.volume = volume
 #   create a method to increase the channel number by 1
     def channel_up(self):
         if self.channel < 120 and self.status:

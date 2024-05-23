249dff13d998596da591e9ba15e9ec05bab417ac
Arvie Lastra <lastraarvie@gmail.com>
2024-05-23 14:51:46 +0800

    Removed unnecessary parameters

--- test_tv.py
+++ test_tv.py
@@ -10,36 +10,37 @@ if tv_status == "y":
     tv_1.turn_on()
 else:
     tv_1.turn_off()
-    
+
+if tv_1.status:    
 # Asks if the user wants to set a channel
-channel = int(input("Enter the channel (1-120): "))
+    channel = int(input("Enter the channel (1-120): "))
 #    call the method to set a channel to this tv
-tv_1.set_channel(channel)
+    tv_1.set_channel(channel)
 
 # Asks if the user wants to set a volume
-volume = int(input("Enter the Volume level (1-7)"))
+    volume = int(input("Enter the Volume level (1-7)"))
 #    call the method to set a volume to this tv
-tv_1.set_volume(volume)
+    tv_1.set_volume(volume)
 
 # Asks if the user wants to increase the channel number by 1
-increase_channel = input("Do you want to increase the channel number? (y/n)").lower()
+    if input("Do you want to increase the channel number? (y/n)").lower() == "y":
 #    call the method to increases the channel by 1
-tv_1.channel_up(increase_channel)
+        tv_1.channel_up()
 
 # Asks if the user wants to decrease the channel number by 1
-decrease_channel = input("Do you want to decrease the channel number? (y/n)").lower()
+    if input("Do you want to decrease the channel number? (y/n)").lower() == "y":
 #    call the method to decrease the channel by 1
-tv_1.channel_down(decrease_channel)
+        tv_1.channel_down()
 
 # Asks if the user wants to increase the volume level by 1
-increase_volume = input("Do you want to increase the volume level? (y/n)").lower()
+    if input("Do you want to increase the volume level? (y/n)").lower() == "y":
 #    call the method to increases the volume level by 1
-tv_1.volume_up(increase_volume)
+        tv_1.volume_up()
 
 # Asks if the user wants to decrease the volume level by 1 
-decrease_volume = input("Do you want to decrease the volume level? (y/n)").lower()
+    if input("Do you want to decrease the volume level? (y/n)").lower() == "y":
 #    call the method to decrease the volume level by 1 
-tv_1.volume_down(decrease_volume)
+        tv_1.volume_down()
 
 # create an object name tv_2
 tv_2 = TV()
@@ -50,36 +51,37 @@ if tv_status == "y":
     tv_2.turn_on()
 else:
     tv_2.turn_off()
-    
+
+if tv_2.status:    
 # Asks if the user wants to set a channel
-channel = int(input("Enter the channel (1-120): "))
+    channel = int(input("Enter the channel (1-120): "))
 #    call the method to set a channel to this tv
-tv_2.set_channel(channel)
+    tv_2.set_channel()
 
 # Asks if the user wants to set a volume
-volume = int(input("Enter the Volume level (1-7)"))
+    volume = int(input("Enter the Volume level (1-7)"))
 #    call the method to set a volume to this tv
-tv_2.set_volume(volume)
+    tv_2.set_volume()
 
 # Asks if the user wants to increase the channel number by 1
-increase_channel = input("Do you want to increase the channel number? (y/n)").lower()
+    if input("Do you want to increase the channel number? (y/n)").lower() == "y":
 #    call the method to increases the channel by 1
-tv_2.channel_up(increase_channel)
+        tv_2.channel_up()
 
 # Asks if the user wants to decrease the channel number by 1
-decrease_channel = input("Do you want to decrease the channel number? (y/n)").lower()
+    if input("Do you want to decrease the channel number? (y/n)").lower() == "y":
 #    call the method to decrease the channel by 1
-tv_2.channel_down(decrease_channel)
+        tv_2.channel_down()
 
 # Asks if the user wants to increase the volume level by 1
-increase_volume = input("Do you want to increase the volume level? (y/n)").lower()
+    if input("Do you want to increase the volume level? (y/n)").lower() == "y":
 #    call the method to increases the volume level by 1
-tv_2.volume_up(increase_volume)
+        tv_2.volume_up()
 
 # Asks if the user wants to decrease the volume level by 1 
-decrease_volume = input("Do you want to decrease the volume level? (y/n)").lower()
+    if input("Do you want to decrease the volume level? (y/n)").lower() == "y":
 #    call the method to decrease the volume level by 1 
-tv_2.volume_down(decrease_volume)
+        tv_2.volume_down()
 
 # Display the tv1 and tv2
 print(f"tv1's channel is {tv_1.get_channel} volume level is {tv_1.get_volume}" + '\n' +
